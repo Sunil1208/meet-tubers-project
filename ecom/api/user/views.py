77bce011d703897a6bac1fa26d7e820c56a0fba2
Sunil Kumar <sunilkumar@LSN-ITs-MacBook-Air.local>
2022-02-23 13:15:47 +0530

    fixed signup issue

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -79,6 +79,6 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         try:
-            [permission() for permission in self.permission_classes_by_action[self.action]]
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
         except KeyError:
             return [permission() for permission in self.permission_classes]
