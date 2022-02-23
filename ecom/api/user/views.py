ee71e6ce924fcca5ae92a1d84a097004385ca44a
Sunil Kumar <sunilkumar@LSN-ITs-MacBook-Air.local>
2022-02-23 11:26:12 +0530

    added generate_session_token logic

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -1,3 +1,7 @@
+import random
 from django.shortcuts import render
 
 # Create your views here.
+
+def generate_session_token(length=10):
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]) for _ in range(length))
