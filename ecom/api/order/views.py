7d23ec856f649d26fecc21d6b0680474ede792c8
Sunil Kumar <sunilkumar@LSN-ITs-MacBook-Air.local>
2022-02-23 23:19:55 +0530

    added logic to check if user is authenticated or not

--- ecom/api/order/views.py
+++ ecom/api/order/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+
+from .serializers import OrderSerializer
+from .models import Order
 
 # Create your views here.
+
+def validate_user_session(id, token):
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(pk=id)
+        if user.session_token == token:
+            return True
+        return False
+    except UserModel.DoesNotExist:
+        return False
